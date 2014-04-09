2ff7f6a559a6596b633b254845505c84a265f034
epha <marco.egbring@epha.ch>
2014-04-09 18:20:53 +0200

    machine

--- app.js
+++ app.js
@@ -3,18 +3,41 @@ var machine = require("./modules/machine");
 
 var test = require("./data/columbia.json");
 
-//machine.train("./data/columbia.json").save("./data/machine.json");
+machine.train("./data/columbia.json").save("./data/machine.json");
 
 //machine.load("./data/machine.json");
 
 machine.talking( function( question )
 {
-  console.log( question );
+    console.log("PARSING..");
+  
+    var symptome = {};
+    var codes = question.trim().split("+").forEach( function(element)
+    {
+       symptome[ element ] = 1;    
+    });
+  
+    var answer = machine.predict( symptome );
+    var cali = machine.predict( {} );
+  
+    var hitliste = [];
+  
+    for( var item in answer )
+    {
+      hitliste.push( { "diagnose":item, "proba": answer[item] - cali[item] } );
+    }
+    hitliste.sort( function( a,b ) { return b.proba-a.proba; });
+  
+    for( var i = 0; i < 3 && i < hitliste.length; i++)
+    {
+      if( hitliste[i].proba > 0.01 )
+      console.log( hitliste[i].proba.toFixed(5), hitliste[i].diagnose );
+    }
 });
 
 machine.exit(function() 
 {
-  console.log('Wanna try again?');
+  console.log('BYE BYE');
   process.exit(0);
 });
 
